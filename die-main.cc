f62d454f76eb248d21263c58990fb6d90e3dc551
tl791619 <tl791619@ohio.edu>
2020-08-28 22:23:15 -0400

    die-main.cc added

---
+++ die-main.cc
@@ -0,0 +1,22 @@
+/*
+* Author: Tatiana 
+* Date: August 28th, 2020
+* Description: Die project main class.
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "die.h"
+using namespace std;
+//function prototypes
+int main() {
+Die d;
+cout << "Face value: "<< d << endl;
+Die d12(12);
+cout << "Face value: "<< d12 << endl;
+d12.setNumSides(24);
+for (size_t i = 0; i < 6; i++) {
+cout << "Roll: " << d12.roll() << endl;
+}
+return 0;
+}
